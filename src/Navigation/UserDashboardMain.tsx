d5a365c967bc2c7c2d1ea96e89e5b650c6a1cd89
John Doe <mohaiminur24@gmail.com>
2023-10-29 13:16:52 +0600

    userDashboard tab bottom is complete

--- src/Navigation/UserDashboardMain.tsx
+++ src/Navigation/UserDashboardMain.tsx
@@ -3,6 +3,8 @@ import UserDashboard from "../screens/dashboard/UserDashboard";
 import SignupPage from "../screens/userverify/SignupPage";
 import Icon from "react-native-vector-icons/Entypo";
 import Ionicons from "react-native-vector-icons/Ionicons";
+import Feather from "react-native-vector-icons/Feather";
+import Loginpage from "../screens/userverify/LoginPage";
 
 const Tab = createBottomTabNavigator();
 
@@ -15,11 +17,12 @@ const UserDashboardMain = () => {
           headerShown: false,
           tabBarStyle: {
             position: "absolute",
-            bottom: 20,
-            right: 20,
-            left: 20,
-            height: 70,
-            borderRadius: 10,
+            bottom: 0,
+            height: 80,
+            paddingBottom: 15,
+            paddingTop: 15,
+            borderTopLeftRadius: 40,
+            borderTopRightRadius: 40,
           },
         }}
       >
@@ -28,16 +31,40 @@ const UserDashboardMain = () => {
           options={{
             tabBarLabel: "Dashboard",
             tabBarIcon: () => <Icon name="home" size={25} color="red" />,
-            tabBarLabelPosition: "beside-icon",
           }}
           component={UserDashboard}
+        />
+        <Tab.Screen
+          name="Create"
+          component={Loginpage}
+          options={{
+            tabBarLabel: "",
+            tabBarIcon: () => (
+              <Feather
+                name="plus"
+                size={30}
+                color="white"
+                style={{
+                  backgroundColor: "blue",
+                  position: "absolute",
+                  borderRadius: 50,
+                  borderColor: "#ededed",
+                  borderWidth: 5,
+                  bottom: 5,
+                  paddingLeft: 20,
+                  paddingTop: 20,
+                  paddingBottom: 12,
+                  paddingRight: 12,
+                }}
+              />
+            ),
+          }}
         />
         <Tab.Screen
           name="signuppage"
           options={{
             tabBarLabel: "Create Event",
             tabBarIcon: () => <Ionicons name="create" size={25} color="red" />,
-            tabBarLabelPosition: "beside-icon",
           }}
           component={SignupPage}
         />
